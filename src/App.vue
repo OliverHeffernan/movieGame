cd6ab17d3ddab23da110572301dc7ec2eb2ced55
OliverHeffernan <oliverheffernan@icloud.com>
2025-04-11 23:37:22 +1200

    added GUI, made changes to the film criteria, added a backup for if the connection fails, general clean up.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,102 @@
 <template>
-	<div id="hello">hello</div>
-	<img id="moviePoster" class="guess0" src="" alt="">
-	<input type="text" id="guessInput">
-	<ul id="hints">
-	</ul>
-	<button @click="doGuess">Guess</button>
+	<div class="vert">
+		<img id="moviePoster" class="guess0" alt="">
+		<div id="loading" v-if="!movieLoaded">Finding a film...</div>
+		<input placeholder="Guess the movie" type="text" id="guessInput">
+		<button id="guessButton" @click="doGuess">Guess</button>
+		<div id="hints">
+		</div>
+	</div>
 </template>
 
-<script setup>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+* {
+	background-color: rgb(25,25,25);
+	color: white;
+	font-family: "Roboto", sans-serif;
+}
 
+#loading {
+	width: min(calc(100vw - 20px), 400px);
+	height: min(calc((100vw - 20px)*1.5), 600px);
+	margin-left: 50vw;
+	transform: translateX(-50%);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+	background-color: black;
+}
+
+#hints {
+	margin-left: 50vw;
+	transform: translateX(-50%);
+	width: min(calc(100vw - 20px), 400px);
+	list-style: none;
+}
+
+#hints p {
+	padding: 10px;
+	border-top: 2px solid rgb(50,50,50);
+}
+
+#guessInput {
+	margin-left: 50vw;
+	transform: translateX(-50%);
+	width: min(calc(100vw - 20px), 400px);
+	border: 2px solid rgb(50,50,50);
+	padding: 10px;
+	border-radius: 5px;
+	font-size: 20px;
+	margin-top: 10px;
+}
+
+#moviePoster {
+	margin-left: 50vw;
+	transform: translateX(-50%);
+	width: min(calc(100vw - 20px), 400px);
+	overflow: hidden;
+	height: auto;
+	clip-path: inset(0);
+}
+
+#guessButton {
+	margin-left: 50vw;
+	transform: translateX(-50%);
+	width: min(calc(100vw - 20px), 400px);
+	padding: 10px;
+	border-radius: 10px;
+	border: none;
+	background-color: #1B82B4;
+	box-shadow: inset 0px -4px 0px rgba(0,0,0,0.5);
+	font-size: 20px;
+	margin-top: 10px;
+}
+
+.vert {
+	display: flex;
+	flex-direction: column;
+}
+
+.vert > * {
+	display: block;
+}
+</style>
+
+<script setup>
+import { ref } from "vue";
+import SampleData from "./classes/sampleData.js";
+const data = new SampleData();
 let guessMovie;
 let credits;
 let director;
 let guessCount = 0;
+
+let connection = true;
+let sampleNumber = 0;
+
+let movieLoaded = ref(false);
 
 function stripPunctuation(str) {
 	let result = String(str).replace(/[^\w\s]/g, '').toLowerCase();
@@ -32,37 +116,62 @@ function doGuess() {
 		console.log("correct");
 	} else {
 		document.getElementById("moviePoster").className = "guess" + guessCount;
-		doHint(guessCount);
+		doHint(guessCount, guess);
 	}
 }
 
 function getHint(count) {
 	if (count == 1) {
-		return "It has the actor " + credits.cast[0].name + " in it.";
+		return "It has actor " + credits.cast[2].name + " in it.";
 	}
 	if (count == 2) {
-		return "It also has actor " + credits.cast[1].name + " in it.";
+		return "The movie was released on " + guessMovie.release_date;
 	}
 	if (count == 3) {
 		return "The director is " + director;
 	}
 	if (count == 4) {
-		return "The movie was released on " + guessMovie.release_date;
+		return "It also has actor " + credits.cast[1].name + " in it.";
+	}
+	if (count == 5) {
+		return "It also has the actor " + credits.cast[0].name + " in it.";
 	}
 }
 
-function doHint(count) {
+function doHint(count, guess) {
+	if (count == 6) {
+		addHint(guess + " is incorrect. The answer is " + guessMovie.title);
+	}
+	else if (count < 6) {
+		addHint(guess + " is incorrect. Hint: " + getHint(count));
+	}
+}
+
+function addHint(text) {
 	const ul = document.getElementById("hints");
-	const li = document.createElement('li');
-	li.innerText = getHint(count);
+	const li = document.createElement('p');
+	li.innerText = text;
 	ul.appendChild(li);
+}
+
+function getRandomInt(a, b) {
+	const min = Math.ceil(Math.min(a, b));
+	const max = Math.floor(Math.max(a, b));
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 async function fetchResults() {
 	try {
+		const controller = new AbortController();
+		const timeout = 5000;
+		const timeoutId = setTimeout(() => controller.abort(), timeout);
+
 		const response = await fetch('https://192.168.68.59:8443/movie', {
 			method: 'GET',
+			signal: controller.signal,
 		});
+
+		clearTimeout(timeoutId);
 
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
@@ -72,12 +181,21 @@ async function fetchResults() {
 		return JSON.parse(text.substring(8));
 	} catch (error) {
 		console.error('Error fetching result:', error);
-		return "ERROR";
+		//return "ERROR";
+		connection = false;
+		//console.log("connection failed");
+		sampleNumber = getRandomInt(0, data.data.length - 1);
+		//console.log(sampleNumber);
+		return data.data[sampleNumber].movie;
 	}
 }
 
 async function fetchCredits(id) {
 	try {
+		if (!connection) {
+			throw new Error('No connection available');
+		}
+
 		const response = await fetch('https://192.168.68.59:8443/credits/' + id, {
 			method: 'GET',
 		});
@@ -88,31 +206,38 @@ async function fetchCredits(id) {
 		const text = await response.text();
 		return JSON.parse(text.substring(8));
 	} catch (error) {
-		console.error('Error fetching credits:', error);
-		return "ERROR";
+		//console.error('Error fetching credits:', error);
+		//return "ERROR";
+		console.log("credits failed");
+		return data.data[sampleNumber];
 	}
 }
 
 async function getMovie() {
 	let movie = await fetchResults();
-	while (movie == null || (movie.success === false || movie.adult || movie.poster_path === null || movie.original_language != "en" || parseInt(movie.release_date.split("-")[0]) < 1970 || movie.vote_average < 8 || movie.vote_count < 10000)) {
+	//while (movie == null || (movie.success === false || movie.adult || movie.poster_path === null || movie.original_language != "en" || parseInt(movie.release_date.split("-")[0]) < 1970 || movie.vote_average < 7 || movie.vote_count < 7000)) {
+		//movie = await fetchResults();
+	//}
+
+	while (movie == null || (movie.success == false || movie.adult || movie.poster_path == null || movie.original_language != "en" || movie.vote_average < 7 || movie.vote_count < 6500)) {
 		movie = await fetchResults();
-		console.log(movie);
 	}
 	guessMovie = movie;
 	credits = await fetchCredits(movie.id);
-	console.log(credits);
+	console.log(JSON.stringify(movie, null, 2));
+	console.log(JSON.stringify(credits, null, 2));
 
+	//console.log(movie.poster_path);
 	document.getElementById("moviePoster").src = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/" + movie.poster_path;
 
 	for (let i = 0; i < credits.crew.length; i++) {
-		if (credits.crew[i].job == "Director") {
+		if (credits.crew[i].job == "Director" || credits.crew[i].known_for_department == "Directing") {
 			director = credits.crew[i].name;
 			break;
 		}
 	}
 
-	console.log(director);
+	movieLoaded.value = true;
 }
 
 getMovie();
---
+++ src/classes/sampleData.js
@@ -0,0 +1,412 @@
+export default class SampleData {
+	constructor() {
+		this.data = [
+			{
+				movie: {
+					adult: false,
+					backdrop_path: "/bdD39MpSVhKjxarTxLSfX6baoMP.jpg",
+					genre_ids: [18, 36, 10752],
+					id: 857,
+					original_language: "en",
+					original_title: "Saving Private Ryan",
+					overview: "As U.S. troops storm the beaches of Normandy, three brothers lie dead on the battlefield, with a fourth trapped behind enemy lines. Range…",
+					popularity: 15.6487,
+					poster_path: "/uqx37cS8cpHg8U35f9U5IBlrCV3.jpg",
+					release_date: "1998-07-24",
+					title: "Saving Private Ryan",
+					video: false,
+					vote_average: 8.218,
+					vote_count: 16194
+				},
+
+				cast: [
+					{adult: false, gender: 2, id: 31, known_for_department: "Acting", name: "Tom Hanks"},
+					{adult: false, gender: 2, id: 3197, known_for_department: "Acting", name: "Tom Sizemore"},
+					{adult: false, gender: 2, id: 12833, known_for_department: "Acting", name: "Edward Burns"},
+					{adult: false, gender: 2, id: 12834, known_for_department: "Acting", name: "Barry Pepper"},
+					{adult: false, gender: 2, id: 6163, known_for_department: "Acting", name: "Adam Goldberg"},
+					{adult: false, gender: 2, id: 12835, known_for_department: "Acting", name: "Vin Diesel"},
+					{adult: false, gender: 2, id: 1771, known_for_department: "Acting", name: "Giovanni Ribisi"},
+					{adult: false, gender: 2, id: 4654, known_for_department: "Acting", name: "Jeremy Davies"},
+					{adult: false, gender: 2, id: 1892, known_for_department: "Acting", name: "Matt Damon"},
+					{adult: false, gender: 2, id: 12836, known_for_department: "Acting", name: "Ted Danson"}
+				],
+				crew: [
+					{adult: false, gender: 2, id: 488, known_for_department: "Directing", name: "Steven Spielberg"},
+					{adult: false, gender: 2, id: 493, known_for_department: "Editing", name: "Michael Kahn"},
+					{adult: false, gender: 2, id: 2486, known_for_department: "Art", name: "Thomas E. Sanders"},
+					{adult: false, gender: 2, id: 6048, known_for_department: "Production", name: "Mark Gordon"},
+					{adult: false, gender: 2, id: 491, known_for_department: "Sound", name: "John Williams"},
+					{adult: false, gender: 2, id: 492, known_for_department: "Camera", name: "Janusz Kamiński"},
+					{adult: false, gender: 2, id: 284, known_for_department: "Production", name: "Gary Levinsohn"},
+					{adult: false, gender: 2, id: 9987, known_for_department: "Production", name: "Ian Bryce"},
+					{adult: false, gender: 1, id: 7849, known_for_department: "Art", name: "Lisa Dean"},
+					{adult: false, gender: 2, id: 488, known_for_department: "Directing", name: "Steven Spielberg"},
+					{adult: false, gender: 1, id: 2215, known_for_department: "Production", name: "Denise Chamian"},
+					{adult: false, gender: 2, id: 12832, known_for_department: "Writing", name: "Robert Rodat"},
+					{adult: false, gender: 2, id: 75804, known_for_department: "Production", name: "Kevin De La Noy"}
+				]
+			},
+			{
+				movie: {
+					adult: false,
+					backdrop_path: "/wXsQvli6tWqja51pYxXNG1LFIGV.jpg",
+					genre_ids: [10751, 16, 18],
+					id: 8587,
+					original_language: "en",
+					original_title: "The Lion King",
+					overview: "Young lion prince Simba, eager to one day become king of the Pride Lands, grows up under the watchful eye of his father Mufasa; all th…",
+					popularity: 25.8472,
+					poster_path: "/sKCr78MXSLixwmZ8DyJLrpMsd15.jpg",
+					release_date: "1994-06-15",
+					title: "The Lion King",
+					video: false,
+					vote_average: 8.3,
+					vote_count: 18664
+				},
+				cast: [
+					{adult: false, gender: 2, id: 4756, known_for_department: "Acting", name: "Matthew Broderick"},
+					{adult: false, gender: 1, id: 20005, known_for_department: "Acting", name: "Moira Kelly"},
+					{adult: false, gender: 2, id: 78729, known_for_department: "Acting", name: "Nathan Lane"},
+					{adult: false, gender: 2, id: 69415, known_for_department: "Acting", name: "Ernie Sabella"},
+					{adult: false, gender: 2, id: 15152, known_for_department: "Acting", name: "James Earl Jones"},
+					{adult: false, gender: 2, id: 16940, known_for_department: "Acting", name: "Jeremy Irons"},
+					{adult: false, gender: 2, id: 8294, known_for_department: "Acting", name: "Robert Guillaume"},
+					{adult: false, gender: 2, id: 10730, known_for_department: "Acting", name: "Rowan Atkinson"},
+					{adult: false, gender: 2, id: 53283, known_for_department: "Acting", name: "Jonathan Taylor Thomas"},
+					{adult: false, gender: 1, id: 56043, known_for_department: "Acting", name: "Niketa Calame-Harris"},
+					{adult: false, gender: 1, id: 2395, known_for_department: "Acting", name: "Whoopi Goldberg"},
+					{adult: false, gender: 2, id: 11159, known_for_department: "Acting", name: "Cheech Marin"},
+					{adult: false, gender: 2, id: 12077, known_for_department: "Acting", name: "Jim Cummings"},
+					{adult: false, gender: 1, id: 58074, known_for_department: "Acting", name: "Madge Sinclair"},
+					{adult: false, gender: 1, id: 723462, known_for_department: "Acting", name: "Zoe Leader"},
+				],
+				crew: [
+					{adult: false, gender: 2, id: 15812, known_for_department: "Writing", name: "Roger Allers"},
+					{adult: false, gender: 1, id: 38792, known_for_department: "Writing", name: "Linda Woolverton"},
+					{adult: false, gender: 1, id: 56041, known_for_department: "Writing", name: "Irene Mecchi"},
+					{adult: false, gender: 2, id: 7933, known_for_department: "Writing", name: "Jonathan Roberts"},
+					{adult: false, gender: 2, id: 56042, known_for_department: "Editing", name: "Ivan Bilancio"},
+					{adult: false, gender: 2, id: 18898, known_for_department: "Directing", name: "Rob Minkoff"},
+					{adult: false, gender: 2, id: 947, known_for_department: "Sound", name: "Hans Zimmer"},
+					{adult: false, gender: 0, id: 226007, known_for_department: "Visual Effects", name: "Vera Pacheco"},
+					{adult: false, gender: 0, id: 1450331, known_for_department: "Visual Effects", name: "James Baxter"},
+					{adult: false, gender: 2, id: 1395352, known_for_department: "Production", name: "Craig Sost"},
+					{adult: false, gender: 2, id: 65534, known_for_department: "Visual Effects", name: "Andreas Deja"},
+					{adult: false, gender: 2, id: 1447376, known_for_department: "Visual Effects", name: "Anthony DeRosa"},
+					{adult: false, gender: 2, id: 1447370, known_for_department: "Visual Effects", name: "Tim Allen"},
+					{adult: false, gender: 2, id: 1455541, known_for_department: "Visual Effects", name: "Larry White"},
+					{adult: false, gender: 0, id: 1447483, known_for_department: "Directing", name: "Rick Moore"},
+					{adult: false, gender: 0, id: 1447436, known_for_department: "Directing", name: "Jennifer Yuan"},
+					{adult: false, gender: 2, id: 1459479, known_for_department: "Crew", name: "Carl Canga"},
+					{adult: false, gender: 0, id: 1447357, known_for_department: "Visual Effects", name: "Rick Maki"}
+				]
+			},
+			{
+				movie: {
+					"adult": false,
+					"backdrop_path": "/neeNHeXjMF5fXoCJRsOmkNGC7q.jpg",
+					"genre_ids": [
+						18,
+						36
+					],
+					"id": 872585,
+					"original_language": "en",
+					"original_title": "Oppenheimer",
+					"overview": "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+					"popularity": 26.6463,
+					"poster_path": "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+					"release_date": "2023-07-19",
+					"title": "Oppenheimer",
+					"video": false,
+					"vote_average": 8.067,
+					"vote_count": 10075
+				},
+				cast: [
+					{ "adult": false, "gender": 2, "id": 2037, "known_for_department": "Acting", "name": "Cillian Murphy", "original_name": "Cillian Murphy", "popularity": 8.8907, "profile_path": "/llkbyWKwpfowZ6C8peBjIV9jj99.jpg", "cast_id": 3, "character": "J. Robert Oppenheimer", "credit_id": "613a940d9653f60043e380df", "order": 0 },
+					{ "adult": false, "gender": 1, "id": 5081, "known_for_department": "Acting", "name": "Emily Blunt", "original_name": "Emily Blunt", "popularity": 5.2689, "profile_path": "/5nCSG5TL1bP1geD8aaBfaLnLLCD.jpg", "cast_id": 161, "character": "Kitty Oppenheimer", "credit_id": "6328c918524978007e9f1a7f", "order": 1 },
+					{ "adult": false, "gender": 2, "id": 1892, "known_for_department": "Acting", "name": "Matt Damon", "original_name": "Matt Damon", "popularity": 6.0323, "profile_path": "/4KAxONjmVq7qcItdXo38SYtnpul.jpg", "cast_id": 108, "character": "Leslie Groves", "credit_id": "6328ad9843250f00830efdca", "order": 2 },
+				],
+				crew: [
+					{adult: false, gender: 2, id: 1234, known_for_department: "Directing", name: "Christopher Nolan"},
+				]
+			},
+			{
+				movie: {
+					"adult": false,
+					"backdrop_path": "/9BBTo63ANSmhC4e6r62OJFuK2GL.jpg",
+					"genre_ids": [
+						878,
+						28,
+						12
+					],
+					"id": 24428,
+					"original_language": "en",
+					"original_title": "The Avengers",
+					"overview": "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!",
+					"popularity": 35.1429,
+					"poster_path": "/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
+					"release_date": "2012-04-25",
+					"title": "The Avengers",
+					"video": false,
+					"vote_average": 7.735,
+					"vote_count": 31521
+				},
+				cast: [
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 3223,
+						"known_for_department": "Acting",
+						"name": "Robert Downey Jr.",
+						"original_name": "Robert Downey Jr.",
+						"popularity": 8.7499,
+						"profile_path": "/5qHNjhtjMD4YWH3UP0rm4tKwxCL.jpg",
+						"cast_id": 46,
+						"character": "Tony Stark / Iron Man",
+						"credit_id": "52fe4495c3a368484e02b251",
+						"order": 0
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 16828,
+						"known_for_department": "Acting",
+						"name": "Chris Evans",
+						"original_name": "Chris Evans",
+						"popularity": 10.0813,
+						"profile_path": "/3bOGNsHlrswhyW79uvIHH1V43JI.jpg",
+						"cast_id": 2,
+						"character": "Steve Rogers / Captain America",
+						"credit_id": "52fe4495c3a368484e02b19b",
+						"order": 1
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 103,
+						"known_for_department": "Acting",
+						"name": "Mark Ruffalo",
+						"original_name": "Mark Ruffalo",
+						"popularity": 4.8665,
+						"profile_path": "/5GilHMOt5PAQh6rlUKZzGmaKEI7.jpg",
+						"cast_id": 307,
+						"character": "Bruce Banner / The Hulk",
+						"credit_id": "5e85e8083344c60015411cfa",
+						"order": 2
+					},
+				],
+				crew: [
+					{adult: false, gender: 2, id: 1234, known_for_department: "Directing", name: "Joss Whedon"},
+				]
+			},
+			{
+				movie: {
+					"adult": false,
+					"backdrop_path": "/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg",
+					"genre_ids": [
+						878,
+						12
+					],
+					"id": 438631,
+					"original_language": "en",
+					"original_title": "Dune",
+					"overview": "Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, must travel to the most dangerous planet in the universe to ensure the future of his family and his people. As malevolent forces explode into conflict over the planet's exclusive supply of the most precious resource in existence-a commodity capable of unlocking humanity's greatest potential-only those who can conquer their fear will survive.",
+					"popularity": 25.0571,
+					"poster_path": "/d5NXSklXo0qyIYkgV94XAgMIckC.jpg",
+					"release_date": "2021-09-15",
+					"title": "Dune",
+					"video": false,
+					"vote_average": 7.782,
+					"vote_count": 13394
+				},
+				cast: [
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 1190668,
+						"known_for_department": "Acting",
+						"name": "Timothée Chalamet",
+						"original_name": "Timothée Chalamet",
+						"popularity": 21.072,
+						"profile_path": "/BE2sdjpgsa2rNTFa66f7upkaOP.jpg",
+						"cast_id": 13,
+						"character": "Paul Atreides",
+						"credit_id": "5b4d01bac3a36823d803cd45",
+						"order": 0
+					},
+					{
+						"adult": false,
+						"gender": 1,
+						"id": 933238,
+						"known_for_department": "Acting",
+						"name": "Rebecca Ferguson",
+						"original_name": "Rebecca Ferguson",
+						"popularity": 6.5539,
+						"profile_path": "/lJloTOheuQSirSLXNA3JHsrMNfH.jpg",
+						"cast_id": 14,
+						"character": "Lady Jessica Atreides",
+						"credit_id": "5b90742fc3a368222e002f41",
+						"order": 1
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 25072,
+						"known_for_department": "Acting",
+						"name": "Oscar Isaac",
+						"original_name": "Oscar Isaac",
+						"popularity": 8.5568,
+						"profile_path": "/dW5U5yrIIPmMjRThR9KT2xH6nTz.jpg",
+						"cast_id": 53,
+						"character": "Duke Leto Atreides",
+						"credit_id": "5c50bc070e0a2612cccedcb3",
+						"order": 2
+					},
+				],
+				crew: [
+					{adult: false, gender: 2, id: 1234, known_for_department: "Directing", name: "Denis Villeneuve"},
+				]
+			},
+			{
+				movie: {
+					"adult": false,
+					"backdrop_path": "/3Rfvhy1Nl6sSGJwyjb0QiZzZYlB.jpg",
+					"genre_ids": [
+						16,
+						12,
+						10751,
+						35
+					],
+					"id": 862,
+					"original_language": "en",
+					"original_title": "Toy Story",
+					"overview": "Led by Woody, Andy's toys live happily in his room until Andy's birthday brings Buzz Lightyear onto the scene. Afraid of losing his place in Andy's heart, Woody plots against Buzz. But when circumstances separate Buzz and Woody from their owner, the duo eventually learns to put aside their differences.",
+					"popularity": 23.9924,
+					"poster_path": "/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg",
+					"release_date": "1995-11-22",
+					"title": "Toy Story",
+					"video": false,
+					"vote_average": 7.968,
+					"vote_count": 18739
+				},
+				cast: [
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 31,
+						"known_for_department": "Acting",
+						"name": "Tom Hanks",
+						"original_name": "Tom Hanks",
+						"popularity": 10.3792,
+						"profile_path": "/eKF1sGJRrZJbfBG1KirPt1cfNd3.jpg",
+						"cast_id": 14,
+						"character": "Woody (voice)",
+						"credit_id": "52fe4284c3a36847f8024f95",
+						"order": 0
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 12898,
+						"known_for_department": "Acting",
+						"name": "Tim Allen",
+						"original_name": "Tim Allen",
+						"popularity": 1.7015,
+						"profile_path": "/woWhZzFILVhYMAvsPL171HjMY0y.jpg",
+						"cast_id": 15,
+						"character": "Buzz Lightyear (voice)",
+						"credit_id": "52fe4284c3a36847f8024f99",
+						"order": 1
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 7167,
+						"known_for_department": "Acting",
+						"name": "Don Rickles",
+						"original_name": "Don Rickles",
+						"popularity": 0.998,
+						"profile_path": "/iJLQV4dcbTUgxlWJakjDldzlMXS.jpg",
+						"cast_id": 16,
+						"character": "Mr. Potato Head (voice)",
+						"credit_id": "52fe4284c3a36847f8024f9d",
+						"order": 2
+					},
+				],
+				crew: [
+					{adult: false, gender: 2, id: 1234, known_for_department: "Directing", name: "John Lasseter"},
+				]
+			},
+			{
+				movie: {
+					"adult": false,
+					"backdrop_path": "/rbZvGN1A1QyZuoKzhCw8QPmf2q0.jpg",
+					"genre_ids": [
+						18,
+						53,
+						9648
+					],
+					"id": 11324,
+					"original_language": "en",
+					"original_title": "Shutter Island",
+					"overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by troubling visions and a mysterious doctor.",
+					"popularity": 20.4984,
+					"poster_path": "/nrmXQ0zcZUL8jFLrakWc90IR8z9.jpg",
+					"release_date": "2010-02-14",
+					"title": "Shutter Island",
+					"video": false,
+					"vote_average": 8.201,
+					"vote_count": 24411
+				},
+				cast: [
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 6193,
+						"known_for_department": "Acting",
+						"name": "Leonardo DiCaprio",
+						"original_name": "Leonardo DiCaprio",
+						"popularity": 12.4012,
+						"profile_path": "/ts9l18VkDSooRGDWIeQegNVHciC.jpg",
+						"cast_id": 2,
+						"character": "Teddy Daniels",
+						"credit_id": "52fe44269251416c7502a8ab",
+						"order": 0
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 103,
+						"known_for_department": "Acting",
+						"name": "Mark Ruffalo",
+						"original_name": "Mark Ruffalo",
+						"popularity": 4.8665,
+						"profile_path": "/5GilHMOt5PAQh6rlUKZzGmaKEI7.jpg",
+						"cast_id": 3,
+						"character": "Chuck Aule",
+						"credit_id": "52fe44269251416c7502a8af",
+						"order": 1
+					},
+					{
+						"adult": false,
+						"gender": 2,
+						"id": 2282,
+						"known_for_department": "Acting",
+						"name": "Ben Kingsley",
+						"original_name": "Ben Kingsley",
+						"popularity": 3.1706,
+						"profile_path": "/vQtBqpF2HDdzbfXHDzR4u37i1Ac.jpg",
+						"cast_id": 4,
+						"character": "Dr. Cawley",
+						"credit_id": "52fe44269251416c7502a8b3",
+						"order": 2
+					},
+				],
+				crew: [
+					{adult: false, gender: 2, id: 1234, known_for_department: "Directing", name: "Martin Scorsese"},
+				]
+			}
+		]
+	}
+}
